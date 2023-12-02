8fbbe3e77f23f24aa81c2eccb0db6ce9049cdaf7
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-12-02 08:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your code here
+let item = document.querySelector('#item-name-input');
+let price = document.querySelector('#item-price-input');
+let btn = document.querySelector('#add');
+let tbody = document.querySelector('tbody');
+let total = document.querySelector('#total');
+let arr = [];
+btn.addEventListener('click', () => {
+	let itemName = item.value;
+	let itemPrice = parseInt(price.value);
+	if(isNaN (itemPrice)){
+		alert('Not a valid Number');
+		return;
+	}
+	arr.push(itemPrice);
+	let tr = document.createElement('tr');
+	let td1 = document.createElement('td');
+	td1.innerText = itemName;
+	let td2 = document.createElement('td');
+	td2.innerText = itemPrice;
+	tr.append(td1, td2);
+	tbody.append(tr);
+	item.value = "";
+	price.value = "";
+	let sum = arr.reduce((i,j) => i+j, 0);
+	total.innerText = sum;
+	
+})
+
